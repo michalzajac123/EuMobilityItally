028559b0f45a705ec6d67e4fb5ed2019c6584bb6
Michal Zajac <emmadajo58@gmail.com>
2025-12-02 13:58:51 +0100

    delte border

--- src/components/FooterView.jsx
+++ src/components/FooterView.jsx
@@ -5,18 +5,13 @@ import { IoMdMail } from "react-icons/io";
 
 export default function FooterView() {
   return (
-    <div className=" flex justify-between  mx-auto mt-10">
-      <div className="img flex bg-[#20b486] hidden md:block flex-col  align-items-center w-1/3 justify-center p-4">
-        <div className="image-container">
-          <img src={Logo} alt="Logo" className="w-75" />
-        </div>
-        <div className="text-container">
-          <p className="text-white text-sm mt-2">
-            © 2024 EU Mobility Italy. All rights reserved.
-          </p>
+    <div className=" flex  fiex justify-between  mx-auto mt-10">
+      <div className="img flex bg-[#20b486] hidden md:flex items-center w-1/3 justify-center  p-4">
+        <div className="image-container flex justify-center items-center">
+          <img src={Logo} alt="Logo" className="w-60" />
         </div>
       </div>
-      <div className="bg-white w-1/3 hidden md:block border-y-1 "></div>
+      <div className="bg-white w-1/3 hidden md:block"></div>
       <div className="info flex w-full md:w-1/3 flex-col p-6 justify-between bg-[#cd212a]">
         <h3 className="text-white text-xl text-center sm:text-start font-bold mb-4 border-b border-white/30 pb-2">
           Contact Us
